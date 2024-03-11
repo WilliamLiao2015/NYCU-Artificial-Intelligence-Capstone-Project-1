25a8b846d6dc0262b8e855aa4086e88b481111d0
袁孟華 <mason1719.cs10@nycu.edu.tw>
2024-03-11 13:56:51 +0800

    Update: move to fixTool folder

--- repair.ts
+++
@@ -1,7 +0,0 @@
-const reviewList = JSON.parse(await Deno.readTextFile("./data/reviews.json")) as ReviewData[]
-const repairedReviewList = reviewList.map(review => {
-  const { conclusion } = review
-  return { ...review, conclusion: String(conclusion) === "1" ? 1 : 0 }
-})
-
-await Deno.writeTextFile("./data/reviews.json", JSON.stringify(repairedReviewList))
